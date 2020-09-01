cb77fa03fd69debf2021bc5db5ea3e1caea4f2eb
Dan Dunai <dunai.dan.a@gmail.com>
2020-09-01 17:50:03 +0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const name = document.querySelector('input[name="name"]');
-const pattern = /^[a-z][A-Z]&/;
-
-//live check, the name should contain 3 and more characters, only letters
-name.addEventListener('keyup', () => {
-    console.log(pattern.test(name));
-});
