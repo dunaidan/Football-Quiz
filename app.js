2616390136d23926fc92be4de4333b235fc65cda
Dan Dunai <dunai.dan.a@gmail.com>
2020-09-01 17:55:57 +0300

    Test Git

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const name = document.querySelector('input[name="name"]');
-const pattern = /^[a-z][A-Z]&/;
+//const pattern = /^[a-z][A-Z]&/;
 
 //live check, the name should contain 3 and more characters, only letters
 name.addEventListener('keyup', () => {
