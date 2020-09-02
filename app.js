0d4523705b6036e69aa762467ffae5b9c1cabc95
Dan Dunai <dunai.dan.a@gmail.com>
2020-09-02 12:44:24 +0300

    live check, name and surname validation

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const name = document.querySelector('input[name="name"]');
-//const pattern = /^[a-z][A-Z]&/;
+const pattern = /^[a-z]{3,}$/i;
+//get all text inputs and save them into an array
+const textInput = document.querySelectorAll('input[type="text"]');
 
-//live check, the name should contain 3 and more characters, only letters
-name.addEventListener('keyup', () => {
-    console.log(pattern.test(name));
+//live check, the name and surname should contain 3 and more characters, only letters
+textInput.forEach(item => {
+ item.addEventListener('keyup', () => {
+     console.log(item);
+    if(pattern.test(item.value)){
+        item.className = 'textSuccess';
+    } else {
+        item.className = 'textError';
+    }
+ });
 });
